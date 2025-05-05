c3888fd7b0bfc7218be1eeaec476e81276ffc6ed
LucasRainha <lucas.rainha.domingues@gmail.com>
2025-05-05 18:34:05 -0300

    criacao do package repositories com a adicao dos dados de aplication.properties para as configuracoes necessarias

---
+++ Java/src/main/java/br/com/motix/repositories/MotorcycleRepository.java
@@ -0,0 +1,22 @@
+package br.com.motix.repositories;
+
+import br.com.motix.models.Motorcycle;
+import jakarta.persistence.QueryHint;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MotorcycleRepository extends JpaRepository<Motorcycle, UUID> {
+
+    @Override
+    @QueryHints(@QueryHint(name = "org.hibernate.cacheable", value = "true"))
+    List<Motorcycle> findAll();
+
+    List<Motorcycle> findAllBySector(String sector);
+
+    List<Motorcycle> findAllByType(String type);
+
+    Motorcycle findByPlate(String plate);
+}
